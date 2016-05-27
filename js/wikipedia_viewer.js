9ef27ef1bbec01ba09ee16b8eb04fc8627ce01ff
4iar <4iar@users.noreply.github.com>
2016-05-27 20:32:54 +0100

    Initial

---
+++ js/wikipedia_viewer.js
@@ -0,0 +1,28 @@
+"use strict"
+
+angular.module('wikipediaViewer', [])
+    .controller("search", function($scope, $http) {
+
+        $scope.parseAndAddResult = function (result) {
+            $scope.results.push({
+                url: encodeUri("http://en.wikipedia.org/wiki/" + result.title),
+                title: result.title,
+                snippet: result.snippet
+            });
+        };
+
+        $scope.searchArticles = function (searchString) {
+            var searchApiBaseUrl = "http://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch=";
+            var searchApiUrl = encodeURI(searchApiBaseUrl + searchString);
+
+            $scope.results = [];
+            $http.get(searchApiUrl)
+                .then(function (result) {
+                    result.query.search.forEach(
+                        $scope.parseAndAddResult(result)
+                    );
+                });
+        };
+        $scope.searchArticles("google");
+    });
+
