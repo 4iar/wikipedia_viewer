d97e1dc020533a0b6a711646c92ba4779a025c52
4iar <4iar@users.noreply.github.com>
2016-05-27 22:17:33 +0100

    Add basic searching functionality

--- js/wikipedia_viewer.js
+++ js/wikipedia_viewer.js
@@ -1,28 +1,26 @@
 "use strict"
 
-angular.module('wikipediaViewer', [])
-    .controller("search", function($scope, $http) {
+angular.module('wikipediaViewer', ['ngSanitize'])
+    .controller("search", function($scope, $http, $sce) {
 
         $scope.parseAndAddResult = function (result) {
             $scope.results.push({
-                url: encodeUri("https://en.wikipedia.org/wiki/" + result.title),
+                url: encodeURI("https://en.wikipedia.org/wiki/" + result.title),
                 title: result.title,
-                snippet: result.snippet
+                snippet: $sce.trustAsHtml(result.snippet)
             });
         };
 
         $scope.searchArticles = function (searchString) {
-            var searchApiBaseUrl = "https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch=";
+            var searchApiBaseUrl = "https://crossorigin.me/https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch=";
             var searchApiUrl = encodeURI(searchApiBaseUrl + searchString);
 
             $scope.results = [];
             $http.get(searchApiUrl)
-                .then(function (result) {
-                    result.query.search.forEach(
-                        $scope.parseAndAddResult(result)
-                    );
+                .then(function (apiRequest) {
+                    apiRequest.data.query.search.forEach(function (result) {
+                        $scope.parseAndAddResult(result);
+                    });
                 });
         };
-        $scope.searchArticles("google");
     });
-
